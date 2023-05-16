4c4e9a263a42426e5bf22389f834e4d594b7f7f6
iDragon02 <nejc.zajc02@gmail.com>
2023-05-16 20:39:43 +0200

    Revert "Created Backend express app for RESTful API database comunication, also added user and package models"
    This reverts commit df8f32e5799041da46741a3d7c0bfd329a3176a2.

--- RAZI/Backend/models/packageModel.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-
-var packageSchema = new Schema({
-	'number' : Number
-});
-
-module.exports = mongoose.model('package', packageSchema);
