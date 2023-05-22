365aaa2cd28ad245ac656afafac5b7ffb1d48af3
iDragon02 <nejc.zajc02@gmail.com>
2023-05-22 22:26:45 +0200

    Added function to convert returned base 64 string to .zip file, unzipped it to get .wav file then play it (tokenFormat: 2)

--- PJ/app/src/main/java/njt/paketnik/MainActivity.kt
+++ PJ/app/src/main/java/njt/paketnik/MainActivity.kt
@@ -1,8 +1,12 @@
 package njt.paketnik
 
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.media.MediaPlayer
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
 import njt.paketnik.databinding.ActivityMainBinding
@@ -16,9 +20,16 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
             //daj v .env
             val apiUrl = "https://api-d4me-stage.direct4.me/sandbox/v1/Access/openbox"
-            val jsonData = "{\"deliveryId\":0,\"boxId\":541,\"tokenFormat\":0,\"terminalSeed\":0,\"isMultibox\":false,\"addAccessLog\":false}"
+            val jsonData = "{\"deliveryId\":0,\"boxId\":541,\"tokenFormat\":2,\"terminalSeed\":0,\"isMultibox\":false,\"addAccessLog\":false}"
 
             lifecycleScope.launch {
                 val resJson:JSONObject
@@ -47,9 +58,8 @@ class MainActivity : AppCompatActivity() {
                         binding.showTxt.text = "napaka"
                     }
                     else{
-                        binding.showTxt.text = resJson["data"].toString()
-                        //todo - decode b64 to zip
-                        //todo - unzip and play sound
+                        binding.showTxt.text = "uspeh"
+                        convertB64ToSound(resJson["data"].toString())
                     }
                 }
                 catch (e:JSONException){
@@ -84,5 +94,72 @@ class MainActivity : AppCompatActivity() {
                 return@withContext ""
             }
         }
+    }
+
+    private fun convertB64ToSound(base64String : String) {
+        val byteArray = Base64.decode(base64String, Base64.DEFAULT)
+
+        try {
+            val zipInputStream = ZipInputStream(ByteArrayInputStream(byteArray))
+            var zipEntry: ZipEntry? = zipInputStream.nextEntry
+            while (zipEntry != null) {
+                val entryName = zipEntry.name
+                if (!zipEntry.isDirectory && entryName.endsWith(".wav")) {
+                    val outputStream = ByteArrayOutputStream()
+                    val buffer = ByteArray(1024)
+                    var length: Int
+                    while (zipInputStream.read(buffer).also { length = it } > 0) {
+                        outputStream.write(buffer, 0, length)
+                    }
+                    val soundBytes = outputStream.toByteArray()
+                    playSoundFile(soundBytes)
+                    break
+                }
+                zipEntry = zipInputStream.nextEntry
+            }
+            zipInputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun saveSoundFile(wavBytes: ByteArray): File {
+        val tempDir = cacheDir
+        val tempFile = File.createTempFile("temp", ".wav", tempDir)
+        val outputStream = FileOutputStream(tempFile)
+        outputStream.write(wavBytes)
+        outputStream.close()
+        return tempFile
+    }
+
+    private fun playSoundFile(soundBytes: ByteArray) {
+        releaseMediaPlayer()
+
+        try {
+            val soundFile = saveSoundFile(soundBytes)
+
+            mediaPlayer = MediaPlayer()
+            mediaPlayer?.setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                    .build()
+            )
+            mediaPlayer?.setDataSource(soundFile.path)
+            mediaPlayer?.prepare()
+            mediaPlayer?.start()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun releaseMediaPlayer() {
+        File(cacheDir, "temp.wav").delete()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releaseMediaPlayer()
     }
 }
