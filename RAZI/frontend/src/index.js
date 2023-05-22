7d69c816a8c2c179130e1ecb8f184c1ac527cde6
Timotej <timotej.sekeres@student.um.si>
2023-05-22 17:33:09 +0200

    Added UI library Chakra

--- RAZI/frontend/src/index.js
+++ RAZI/frontend/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ChakraProvider, ColorModeScript, CSSReset} from '@chakra-ui/react'
+import customTheme from './style/theme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={customTheme}>
+        <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
+        <CSSReset />
+            <App />
+
+    </ChakraProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
